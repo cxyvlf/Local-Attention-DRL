77bdde645367d92b766b34bd0ae77695623266c5
cxyvlf <cxyvlf@gmail.com>
2019-12-14 20:50:52 +0800

    nn init function

--- agents/a2c_ppo_acktr/utils.py
+++ agents/a2c_ppo_acktr/utils.py
@@ -10,8 +10,9 @@ class AddBias(nn.Module):
     pass
 
 def init(module, weight_init, bias_init, gain=1):
-    # TO DO
-    pass
+    weight_init(module.weight.data, gain=gain)
+    bias_init(module.bias.data)
+    return module
 
 def cleanup_log_dir(log_dir):
     try:
@@ -20,3 +21,8 @@ def cleanup_log_dir(log_dir):
         files = glob.glob(os.path.join(log_dir, '*.monitor.csv'))
         for f in files:
             os.remove(f)
+
+def update_linear_schedule(optimizer, epoch, total_num_epochs, initial_lr):
+    lr = initial_lr - (initial_lr * (epoch / float(total_num_epochs)))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
