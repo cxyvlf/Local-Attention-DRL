8868e3a41c2d81a853b8de0e83668c10d5e72079
cxyvlf <cxyvlf@gmail.com>
2019-12-17 19:21:03 +0800

    finish CNN actor-critic, a2c framework

--- agents/a2c_ppo_acktr/model.py
+++ agents/a2c_ppo_acktr/model.py
@@ -16,7 +16,7 @@ class Policy(nn.Module):
         super(Policy, self).__init__()
         if base_kwargs is None:
             base_kwargs = {}
-        if base in None:
+        if base is None:
             if len(obs_shape) == 3:
                 base = CNNBase
             elif len(obs_shape) == 1:
@@ -26,52 +26,109 @@ class Policy(nn.Module):
 
         self.base = base(obs_shape[0], **base_kwargs)
 
-        if 
+        if action_space.__class__.__name__ == "Discrete":
+            num_outputs = action_space.n 
+            self.dist = Categorical(self.base.output_size, num_outputs)
+        else:
+            raise NotImplementedError
 
     @property
     def is_recurrent(self):
-        pass
+        return self.base.is_recurrent
 
     @property
     def recurrent_hidden_state_size(self):
-        pass
+        return self.base.recurrent_hidden_state_size
 
     def forward(self, inputs, rnn_hxs, masks):
-        pass
+        raise NotImplementedError
 
     def act(self, inputs, rnn_hxs, masks, deterministic=False):
-        pass
+        value, actor_features, rnn_hxs = self.base(inputs, rnn_hxs, masks)
+        dist = self.dist(actor_features)
+
+        if deterministic:
+            action = dist.mode()
+        else:
+            action = dist.sample()
+
+        action_log_probs = dist.log_probs(action)
+        dist_entropy = dist.entropy().mean()
+
+        return value, action, action_log_probs, rnn_hxs
 
     def get_value(self, inputs, rnn_hxs, masks):
-        pass
+        value, _ , _ = self.base(inputs, rnn_hxs, masks)
+        return value
 
     def evaluate_actions(self, inputs, rnn_hxs, masks, action):
-        pass
+        value, actor_features, rnn_hxs = self.base(inputs, rnn_hxs, masks)
+        dist = self.dist(actor_features)
+
+        action_log_probs = dist.log_probs(action)
+        dist_entropy = dist.entropy().mean()
+
+        return value, action_log_probs, dist_entropy, rnn_hxs
 
 class NNBase(nn.Module):
     def __init__(self, recurrent, recurrent_input_size, hidden_size):
-        pass
+        super(NNBase, self).__init__()
+
+        self._hidden_size  = hidden_size
+        self._recurrent = recurrent
+
+        if recurrent:
+            self.gru = nn.GRU(recurrent_input_size, hidden_size)
+            for name, param in self.gru.named_parameters():
+                if 'bias' in name:
+                    nn.init.constant_(param, 0)
+                elif 'weight' in name:
+                    nn.init.orthogonal_(param)
+    
     @property
     def is_recurrent(self):
-        pass
+        return self.is_recurrent
     
     @property
     def recurrent_hidden_state_size(self):
-        pass
+        if self._recurrent:
+            return self._hidden_size
+        return 1
 
     @property
     def output_size(self):
-        pass
+        return self._hidden_size
 
     def _forward_gru(self, x, hxs, masks):
         pass
 
 class CNNBase(NNBase):
     def __init__(self, num_inputs, recurrent=False, hidden_size=512):
-        pass
+        super(CNNBase, self).__init__(recurrent, hidden_size, hidden_size)
+
+        init_ = lambda m: init(m, nn.init.orthogonal_, lambda x: nn.init.
+                                constant_(x, 0), nn.init.calculate_gain('relu'))
+        
+        self.main = nn.Sequential(
+            init_(nn.Conv2d(num_inputs, 32, 8, stride=4)), nn.ReLU(),
+            init_(nn.Conv2d(32, 64, 4, stride=2)), nn.ReLU(),
+            init_(nn.Conv2d(64, 32, 3, stride=1)), nn.ReLU(), Flatten(),
+            init_(nn.Linear(32 * 7 * 7, hidden_size)), nn.ReLU()
+        )
+
+        init_ = lambda m: init(m, nn.init.orthogonal_, lambda x: nn.init.constant_(x, 0))
+
+        self.critic_linear = init_(nn.Linear(hidden_size, 1))
+
+        self.train()
 
     def forward(self, inputs, rnn_hxs, masks):
-        pass
+        x = self.main(inputs / 255.0)
+
+        if self.is_recurrent:
+            x, rnn_hxs = self._forward_gru(x, rnn_hxs, masks)
+
+        return self.critic_linear(x), x, rnn_hxs
 
 
 class MLPBase(NNBase):
